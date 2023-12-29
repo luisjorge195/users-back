dce0cebb90a31d59e3ca2fc1edc0e548f58015d2
luis <luisjorgepedroza@hotmail.com>
2023-12-29 03:58:58 -0500

    fix origin cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'users-tau-three.vercel.app',
+    origin: 'https://users-tau-three.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
